55686f8985d322f544b4d0f169091c2cf5eb0ca6
Claudio Barca <claudio.barca@teoresigroup.com>
2022-11-18 11:04:39 +0100

    change path where find  test files

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { configDefaults, defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['./src/test/**/*.{test,spec}.{ts,mts,cts,jsx,tsx}'],
     exclude: [...configDefaults.exclude, 'packages/template/*'],
     reporters: 'verbose'
   }
